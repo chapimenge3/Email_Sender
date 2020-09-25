cd1eab90a4c1dfd708084af733a4c0c6355d101c
chapi <chapi@localhost.localdomain>
2020-09-25 00:02:15 -0400

    slight change on naming

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="email_sender-chapimenge", # Replace with your own username
-    version="0.0.1",
+    name="email_sender", 
+    version="0.1",
     author="Temkin Mengistu",
     author_email="chapimenge3@gmail.com",
-    description="Easy Email sender package",
+    description="Easy Email sender Python package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chapimenge3/Email_Sender",
